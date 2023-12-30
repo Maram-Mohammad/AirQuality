ec017944352d74e64a8757e9b99b3a5ed1463de3
Maram Farrag <maram.mohammad@destiny.eu>
2023-12-30 06:54:42 +0200

    fix swagger

--- src/modules/airQuality/infrastructure/webApis/airQualityRoutes.ts
+++ src/modules/airQuality/infrastructure/webApis/airQualityRoutes.ts
@@ -1,39 +1,13 @@
 import { Router } from 'express';
 import { AirQualityController } from './airQualityController';
-const router = Router();
+export const airQualityRouter = Router();
 
-// /**
-//  * @swagger
-//  * /air-quality:
-//  *   get:
-//  *     summary: Get air quality information.
-//  *     parameters:
-//  *       - name: latitude
-//  *         in: query
-//  *         description: Latitude of the location.
-//  *         required: true
-//  *         type: number
-//  *       - name: longitude
-//  *         in: query
-//  *         description: Longitude of the location.
-//  *         required: true
-//  *         type: number
-//  *     responses:
-//  *       200:
-//  *         description: Successful response
-//  *         content:
-//  *           application/json:
-//  *             example:
-//  *               airQuality: 75
-//  */
-router.get('/', AirQualityController.getAll);
-router.get('/test', AirQualityController.test);
-router.get('/nearest-city-pollution', AirQualityController.getNearestCityPollution);
-router.get('/most-polluted-time', AirQualityController.getMostPollutedTime);
-router.get('/:id', AirQualityController.getById);
-router.post('/', AirQualityController.create);
-router.put('/:id', AirQualityController.update);
-router.delete('/:id', AirQualityController.delete);
-
-export default router;
+airQualityRouter.get('/', AirQualityController.getAll);
+airQualityRouter.get('/test', AirQualityController.test);
+airQualityRouter.get('/nearest-city-pollution', AirQualityController.getNearestCityPollution);
+airQualityRouter.get('/most-polluted-time', AirQualityController.getMostPollutedTime);
+airQualityRouter.get('/:id', AirQualityController.getById);
+airQualityRouter.post('/', AirQualityController.create);
+airQualityRouter.put('/:id', AirQualityController.update);
+airQualityRouter.delete('/:id', AirQualityController.delete);
 
--- src/swagger.ts
+++ src/swagger.ts
@@ -4,8 +4,8 @@ import path from "path";
 const doc = {
     info: {
         version: 'v1.0.0',
-        title: 'Swagger AirQuality Project',
-        description: 'Implementation of Swagger with TypeScript'
+        title: 'Air Quality API',
+        description: 'API for managing air quality data',
     },
     servers: [
         {
@@ -19,12 +19,65 @@ const doc = {
                 type: 'http',
                 scheme: 'bearer',
             }
+        },
+        schemas: {
+            'Weather': {
+                'type': 'object',
+                'properties': {
+                    'ts': {'type': 'string', 'format': 'date-time'},
+                    'tp': {'type': 'number'},
+                    'pr': {'type': 'number'},
+                    'hu': {'type': 'number'},
+                    'ws': {'type': 'number'},
+                    'wd': {'type': 'number'},
+                    'ic': {'type': 'string'},
+                }
+            },
+            'PollKeys': {
+                'type': 'object',
+                'properties': {
+                    'conc': {'type': 'number'},
+                    'aqius': {'type': 'number'},
+                    'aqicn': {'type': 'number'},
+                }
+            },
+            'Pollution': {
+                'type': 'object',
+                'properties': {
+                    'ts': {'type': 'string', 'format': 'date-time'},
+                    'aqius': {'type': 'number'},
+                    'mainus': {'type': 'string'},
+                    'aqicn': {'type': 'number'},
+                    'maincn': {'type': 'string'},
+                    'p1': {'$ref': '#/components/schemas/PollKeys'},
+                    'n2': {'$ref': '#/components/schemas/PollKeys'},
+                    's2': {'$ref': '#/components/schemas/PollKeys'},
+                    'co': {'$ref': '#/components/schemas/PollKeys'},
+                }
+            },
+            'IAirQuality': {
+                'type': 'object',
+                'properties': {
+                    'id': {'type': 'string'},
+                    'city': {'type': 'string'},
+                    'state': {'type': 'string'},
+                    'country': {'type': 'string'},
+                    'lat': {'type': 'number'},
+                    'lon': {'type': 'number'},
+                    'weather': {'$ref': '#/components/schemas/Weather'},
+                    'pollution': {'$ref': '#/components/schemas/Pollution'},
+                    'createdAt': {'type': 'string', 'format': 'date-time'},
+                    'updatedAt': {'type': 'string', 'format': 'date-time'},
+                }
+            }
         }
     }
 };
-// const routersPath = path.resolve(__dirname, './app.router.js');
-// console.log("routersPath>>>>>>>", routersPath);
 const outputFile = './swagger_output.json';
 const endpointsFiles = ['./src/app.router.ts'];
 
 swaggerAutogen({openapi: '3.0.0'})(outputFile, endpointsFiles, doc);
+
+
+
+
